6a38c4f0517310265ad80646f8f741db15942bb9
Jamalhmahmud <jamalskype24@gmail.com>
2021-01-05 09:38:01 -0800

    first commit

---
+++ BusManagement/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-BFEEQP3;Initial Catalog=crud;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into user values(@Bus Number,@Driver Name,@Route)",con);
+            cmd.Parameters.AddWithValue("@Bus Number", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Driver Name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Route", textBox3.Text);
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            MessageBox.Show("Saved");
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-BFEEQP3;Initial Catalog=crud;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update user set Bus Number=@Bus Number, Driver Name=@Driver Name, Route=@Route",con); cmd.Parameters.AddWithValue("@Bus Number", int.Parse(textBox1.Text));
+
+            cmd.Parameters.AddWithValue("@Bus Number", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Driver Name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Route", textBox3.Text);
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            MessageBox.Show("Updated");
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-BFEEQP3;Initial Catalog=crud;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete user where Bus Number=@Bus Number",con);
+            cmd.Parameters.AddWithValue("@Bus Number", int.Parse(textBox1.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Deleted");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-BFEEQP3;Initial Catalog=crud;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from user", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+    }
+}
